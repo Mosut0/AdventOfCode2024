cff192f864113f32c3a59dbd8d4ca5393210dd18
Mostafa <mos.yas123@gmail.com>
2024-12-01 18:32:31 -0500

    smthg

--- Day1/part2.py
+++ Day1/part2.py
@@ -8,16 +8,16 @@ def historian_hysteria():
     with open(input_file) as input:
         for line in input:
             nums = line.split()
-            left_list.setdefault(int(nums[0]), 0)
+            left_list.setdefault(int(nums[0]), [0, 0])[0] += 1
             right_list.append(int(nums[1]))
     
     for num in right_list:
         if num in left_list:
-            left_list[num] += 1
+            left_list[num][1] += 1
 
     sum = 0
     for k, v in left_list.items():
-        sum += k * v
+        sum += k * v[0] * v[1]
 
     return sum
 
